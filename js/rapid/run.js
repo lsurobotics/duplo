123ace176a67f13c891709543259641110e19edb
Knaoinr <knaoinr@gmail.com>
2021-01-26 17:30:10 -0500

    Stops the generated code from being repeated twice

--- js/rapid/run.js
+++ js/rapid/run.js
@@ -9,10 +9,5 @@ Blockly.Rapid['custom_start'] = function (block) {
     // However, the function's statements are siblings of the block, rather than children.
     // The scrub_ method in the generator needs an exception for this block, or else the sibling
     // blocks will be included twice.
-    var nextBlock = nextInSplitStack(block);
-    var mainCode = Blockly.Rapid.blockToCode(nextBlock);
-    var code = "ID:" + block.id + " PROC main()\n";
-    code += mainCode;
-    code += "ENDPROC\n";
-    return code;
+    return "ID:" + block.id + " PROC main()\n";
 };
