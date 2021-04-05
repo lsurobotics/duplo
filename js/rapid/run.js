7c35d1503c3460f259c2b2e93889a26b4c123607
hollingsworre <hollingsworre@mymail.vcu.edu>
2021-04-05 07:58:16 -0400

    Fixed variable declaration order in and webview address

--- js/rapid/run.js
+++ js/rapid/run.js
@@ -13,16 +13,16 @@ Blockly.Rapid['custom_start'] = function (block) {
     /**
      * Procedure to check for proper calibration of the gripper needs to be included at 
      * the top of the module, so it is added in here.
-     */
-    const enableLeadThrough = "SetLeadThrough \\On \\NoStopMove;\n";    //command to enable lead through when execution is halted
+     */ 
     const leftArmPos = "VAR robtarget LeftArmPos;\n"; //used to find current position of left arm
     const rightArmPos = "VAR robtarget RightArmPos;\n";   //used to find current position of right arm
+    const gripperVariable = "Var bool isCalibrated := FALSE;\n";   
+    const enableLeadThrough = "SetLeadThrough \\On \\NoStopMove;\n";    //command to enable lead through when execution is halted
 
-    var CalibrateGripper = "Var bool isCalibrated := FALSE;\n";
-    CalibrateGripper    += "isCalibrated := g_IsCalibrated();\n";
+    var CalibrateGripper = "isCalibrated := g_IsCalibrated();\n";
     CalibrateGripper    += "IF isCalibrated = FALSE THEN\n";
     CalibrateGripper    += "  g_Init \\Calibrate;\n";
     CalibrateGripper    += "ENDIF\n";
 
-    return "PROC main()\n" + leftArmPos + rightArmPos + enableLeadThrough + CalibrateGripper;
+    return "PROC main()\n" + leftArmPos + rightArmPos + gripperVariable + enableLeadThrough + CalibrateGripper;
 };
